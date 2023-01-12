87706f511e79fe3222aafe0f834df361fb730e79
Sam Mitchell <samworth27@gmail.com>
2023-01-12 12:38:56 +1000

    cleaned up day 1

--- aoc_2022/day01/main.py
+++ aoc_2022/day01/main.py
@@ -15,7 +15,22 @@ def calorie_finder(input, return_top_n=3, verbose = True):
         current_elf_total += row
     return [heap.remove_max() for i in range(return_top_n)]
 
-
+def main(part1=True, expected_result = ''):
+    start = perf_counter()
+    
+    # Collect data from input file
+    data = parse_input(example=False, function=parse_function)
+    parse_complete = perf_counter()
+    # Run Calculate the sum of the largest n elves
+    results = sum(calorie_finder(data,1 if part1 else 3))
+    
+    complete = perf_counter()
+    total_time = complete - start
+    interval_1 = parse_complete - start
+    interval_2 = complete - parse_complete
+    print(f"Part {1 if part1 else 2} Complete:\n\tTotal Time: {total_time}\n\tParsing Time: {interval_1}\n\tProcessing Time: {interval_2}")
+    print(f"Results: {results}. Expected {expected_result}\n")
+    
 if __name__ == "__main__":
 
     def parse_function(x): return 'end_elf' if x == '' else int(x)
@@ -26,26 +41,9 @@ if __name__ == "__main__":
     print(f"\nTest Answer: {test_results}. Expected {24000}\n")
     
     # Part 1
-    p1_start = perf_counter()
-    p1_data = parse_input(example=False, function=parse_function)
-    p1_parse_complete = perf_counter()
-    p1_results = sum(calorie_finder(p1_data,1))
-    p1_complete = perf_counter()
-    p1_total_time = p1_complete - p1_start
-    p1_interval_1 = p1_parse_complete - p1_start
-    p1_interval_2 = p1_complete - p1_parse_complete
-    print(f"Part 1 Complete:\n\tTotal Time: {p1_total_time}\n\tParsing Time: {p1_interval_1}\n\tProcessing Time: {p1_interval_2}")
-    print(f"Results: {p1_results}. Expected 72718\n")
+    main(part1 = True, expected_result='72718')
     
     # Part 2
-    p2_start = perf_counter()
-    p2_data = parse_input(example=False, function=parse_function)
-    p2_parse_complete = perf_counter()
-    p2_results = sum(calorie_finder(p2_data,3))
-    p2_complete = perf_counter()
-    p2_total_time = p2_complete - p2_start
-    p2_interval_1 = p2_parse_complete - p2_start
-    p2_interval_2 = p2_complete - p2_parse_complete
-    print(f"Part 2 Complete:\n\tTotal Time: {p2_total_time}\n\tParsing Time: {p2_interval_1}\n\tProcessing Time: {p2_interval_2}")
-    print(f"Results: {p2_results}. Expected 213089")
+    main(part1 = False, expected_result='213089')
+
     
