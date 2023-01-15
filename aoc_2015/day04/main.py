fa6b6e6ca357e8c687eadded31399f95abe053d6
Sam Mitchell <samworth27@gmail.com>
2023-01-15 19:10:40 +1000

    day 5 complete

---
+++ aoc_2015/day04/main.py
@@ -0,0 +1,36 @@
+from util.inputs import parse_input
+from hashlib import md5
+
+
+def process(input:str):
+    five = '00000'
+    six = '000000'
+    five_at = None
+    i = 0
+    while True: 
+        first_five = md5((input + str(i)).encode("utf-8")).hexdigest()[:5]
+        if first_five == five:
+            five_at = i
+            print(f"Five found at {i}")
+            break
+        i += 1
+    while True: 
+        first_six = md5((input + str(i)).encode("utf-8")).hexdigest()[:6]
+        if first_six == six:
+            print(f"Six found at {i}")
+            return(five_at, i)
+        i += 1
+
+def test():
+    for data, expected1 in zip(parse_input(True,'example'),parse_input(True,'expected')):
+        result1, result2 = process(data)
+        print(f"Part 1 result: {result1}, expected: {expected1}, match = {result1 == int(expected1)}")
+    
+def main():
+    for data in parse_input():
+        result1,result2 = process(data)
+        print(f"Part 1 result: {result1}, Part 2 result: {result2}")
+    
+if __name__ == '__main__':
+    test()
+    main()
