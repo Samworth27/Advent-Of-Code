2a15a1eedaf8964d565a3f5d7143e4d1b6fb4782
Sam Mitchell <samworth27@gmail.com>
2023-01-15 18:28:06 +1000

    completed day 03

---
+++ aoc_2015/day03/main.py
@@ -0,0 +1,50 @@
+from util.inputs import parse_input
+from util.vector import Vector
+
+move_lookup = {
+    '^': Vector.NORTH,
+    '>': Vector.EAST,
+    'v': Vector.SOUTH,
+    '<':Vector.WEST
+    }
+def part1(input):
+    position = Vector(0,0)
+    visited = set([Vector(0,0)])
+    for inst in input:
+        position += move_lookup[inst]
+        visited.add(position.copy)
+    return len(visited)
+
+def part2(input):
+    position1 = Vector(0,0)
+    position2 = Vector(0,0)
+    robot_santa = False
+    visited = set([Vector(0,0)])
+    for inst in input:
+        move = move_lookup[inst]
+        if robot_santa:
+            position2 += move
+            visited.add(position2)
+            robot_santa = False
+        else:
+            position1 += move
+            visited.add(position1)
+            robot_santa = True
+    return len(visited)
+
+def test():
+    for data, (expected1, expected2) in zip(parse_input(True,'example'),parse_input(True,'expected',lambda x:x.split())):
+        result1 = part1(data)
+        result2 = part2(data)
+        print(f"Result 1: {result1}, expected: {expected1}, match = {result1 == int(expected1)}")
+        print(f"Result 2: {result2}, expected: {expected2}, match = {result2 == int(expected2)}")
+    
+def main():
+    for data in parse_input():
+        result1 = part1(data)
+        result2 = part2(data)
+        print(f"Part 1 result: {result1}, Part 2 result: {result2}")
+    
+if __name__ == '__main__':
+    test()
+    main()
